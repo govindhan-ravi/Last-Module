00822035f5e8cb56b540e3f7634ca01b47240df5
govindhan-ravi <govindhanr7867@gmail.com>
2026-01-08 20:04:02 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  cloud {
+    organization = "Arun-Kumar-Ravi-4824"
+
+    workspaces {
+      name = "tf-cloud-s3"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+}
